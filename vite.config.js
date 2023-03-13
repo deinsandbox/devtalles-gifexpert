5594b9a4939b8246491f990d2541ecfe33e52ca1
equiman <equiman@users.noreply.github.com>
2023-03-12 20:41:51 -0500

    add vite project additional configurations

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,29 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: {
+    globals: true,
+    resolveSnapshotPath: (testPath, snapExtension) => testPath + snapExtension,
+    reporters: ['verbose'],
+    coverage: {
+      all: true,
+      reporter: ['text', 'html', 'lcov'],
+      include: ['**/src/**/*.{js,jsx,ts,tsx}'],
+      exclude: [
+        '**/src/main.{js,jsx,ts,tsx}',
+        '**/*.types.{ts,tsx}',
+        '**/*.test.{js,jsx,ts,tsx}',
+        '**/src/vite-env*',
+      ],
+      statements: 0,
+      branches: 0,
+      functions: 0,
+      lines: 0,
+    },
+    environment: 'jsdom',
+    setupFiles: './vitest/setup.ts',
+  }
 })
