ddbbd723843adddae3f993b944ef86a1c561d789
equiman <equiman@users.noreply.github.com>
2023-03-28 10:21:35 -0500

    add build paths for GHP

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,10 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "./",
+  build: {
+    outDir: './docs'
+  },
   test: {
     globals: true,
     resolveSnapshotPath: (testPath, snapExtension) => testPath + snapExtension,
