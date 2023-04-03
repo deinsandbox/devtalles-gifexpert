32b454da9b2148a29d847d702501bfb47bd37159
equiman <equiman@users.noreply.github.com>
2023-04-03 01:58:29 -0500

    using beforeEach and afterEach

--- src/components/AddCategory.test.jsx
+++ src/components/AddCategory.test.jsx
@@ -1,14 +1,22 @@
 import { render, screen } from '@testing-library/react'
-import { it, expect, describe, vi } from 'vitest'
+import { beforeEach, afterEach, it, expect, describe, vi } from 'vitest'
 import user from '@testing-library/user-event'
 import AddCategory from './AddCategory'
 
 describe('<AddCategory/>', () => {
+  const mockHandleAddCategory = vi.fn()
+
+  beforeEach(() => {
+    user.setup()
+  })
+
+  afterEach(() => {
+    vi.clearAllMocks()
+    vi.resetAllMocks()
+  })
+
   it('should change the value on the input', async () => {
     const category = 'Saitama'
-    user.setup()
-    const mockHandleAddCategory = vi.fn()
-
     render(<AddCategory handleAddCategory={mockHandleAddCategory} />)
     const input = screen.getByRole('textbox')
     await user.type(input, category)
@@ -18,9 +26,6 @@ describe('<AddCategory/>', () => {
 
   it('should call the callback if the input has a value', async () => {
     const category = 'Monkey D. Luffy'
-    user.setup()
-    const mockHandleAddCategory = vi.fn()
-
     render(<AddCategory handleAddCategory={mockHandleAddCategory} />)
     const input = screen.getByRole('textbox')
     await user.type(input, `${category}{enter}`)
@@ -31,8 +36,6 @@ describe('<AddCategory/>', () => {
   })
 
   it('should not call the callback if the input is empty', async () => {
-    const mockHandleAddCategory = vi.fn()
-
     render(<AddCategory handleAddCategory={mockHandleAddCategory} />)
     const input = screen.getByRole('textbox')
     await user.type(input, '{enter}')
